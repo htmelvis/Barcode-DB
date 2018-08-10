2df74fc3f69e2a5d74cf605f7e3352e190dd7d68
htmelvis <ed@htmelvis.com>
2018-08-10 13:34:09 -0400

    user pools added and cloud-api

--- src/Application.js
+++ src/Application.js
@@ -1,71 +1,65 @@
 import React, { Component } from 'react';
-import NewGrudge from './NewGrudge';
-import Grudges from './Grudges';
 import './Application.css';
 import { API } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
 
+// {
+// "gTin": "INSERT VALUE HERE",
+//   "lastPrintDate": "INSERT VALUE HERE",
+//     "printCount": "INSERT VALUE HERE",
+//       "productId": "INSERT VALUE HERE",
+//         "productName": "INSERT VALUE HERE",
+//           "upcUrl": "INSERT VALUE HERE"
+// }
 
 class Application extends Component {
   state = {
-    grudges: [],
+    products: [],
   };
 
   componentDidMount() {
     console.log('I AM NOW MOUNTED!');
     // API NAME, ENDPOINT, WILL RETURN A PROMISE
-    API.get('grudgesCRUD', '/grudges').then(grudges => {
-      this.setState({ grudges });
+    API.get('productListingsCRUD', '/products').then(products => {
+      this.setState({ products });
     });
   }
 
-  addGrudge = grudge => {
-    API.post('grudgesCRUD', '/grudges', { body: grudge }).then(() => {
-      this.setState({ grudges: [grudge, ...this.state.grudges] });
+  addProduct = product => {
+    API.post('productListingsCRUD', '/products', { body: product }).then(() => {
+      this.setState({ products: [product, ...this.state.products] });
     });
   };
 
-  removeGrudge = grudge => {
-    API.del('grudgesCRUD', `/grudges/${grudge.id}`).then(() => {
+  removeProduct = product => {
+    API.del('productListingsCRUD', `/products/${product.id}`).then(() => {
       this.setState({
-        grudges: this.state.grudges.filter(other => other.id !== grudge.id),
+        products: this.state.products.filter(other => other.id !== product.id),
       });
     });
   };
 
-  toggle = grudge => {
-    const updatedGrudge = { ...grudge, avenged: !grudge.avenged };
+  toggle = product => {
+    // const updatedproduct = { ...product, avenged: !product.avenged };
 
     // PUT IN APP.JS IN BACKEND IN ORDER TO SEE DETAILS ** 
-    API.put('grudgesCRUD', '/grudges', { body: updatedGrudge }).then(() => {
-      const othergrudges = this.state.grudges.filter(
-        other => other.id !== grudge.id,
+    API.put('productListingsCRUD', '/products', { body: updatedproduct }).then(() => {
+      const otherproducts = this.state.products.filter(
+        other => other.id !== product.id,
       );
-      this.setState({ grudges: [updatedGrudge, ...othergrudges] });
+      this.setState({ products: [updatedproduct, ...otherproducts] });
     });
 
   };
 
   render() {
-    const { grudges } = this.state;
-    const unavengedgrudges = grudges.filter(grudge => !grudge.avenged);
-    const avengedgrudges = grudges.filter(grudge => grudge.avenged);
+    // const { products } = this.state;
+    // const unavengedproducts = products.filter(product => !product.avenged);
+    // const avengedproducts = products.filter(product => product.avenged);
 
     return (
       <div className="Application">
-        <NewGrudge onSubmit={this.addGrudge} />
-        <Grudges
-          title="Unavenged Grudges"
-          grudges={unavengedgrudges}
-          onCheckOff={this.toggle}
-          onRemove={this.removeGrudge}
-        />
-        <Grudges
-          title="Avenged Grudges"
-          grudges={avengedgrudges}
-          onCheckOff={this.toggle}
-          onRemove={this.removeGrudge}
-        />
+        Hello, World
       </div>
     );
   }
